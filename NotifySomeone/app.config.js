3237e72ae16443d489949fee9f57dcf057e04344
atorres-abona <32295393+atorres-abona@users.noreply.github.com>
2017-09-26 14:06:09 +0200

    Add files via upload

---
+++ NotifySomeone/app.config.js
@@ -0,0 +1,18 @@
+﻿// NOTE object below must be a valid JSON
+window.NotifySomeone = $.extend(true, window.NotifySomeone, {
+    "config": {
+        "layoutSet": "navbar",
+        "navigation": [
+            {
+                title: "Home",
+                onExecute: "#home",
+                icon: "home"
+            },
+            {
+                title: "About",
+                onExecute: "#about",
+                icon: "info"
+            }
+        ]
+    }
+});
